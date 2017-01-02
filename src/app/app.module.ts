c674525c890942818a16454618c9e1f829bb18ea
maxim <maxim@webro.com.ua>
2017-01-02 22:28:07 +0200

    routes examples

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,34 @@ import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
 
+import {
+    RouterModule,
+    Routes
+} from '@angular/router';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { AboutComponentComponent } from './about-component/about-component.component';
+import { ContactComponentComponent } from './contact-component/contact-component.component'
+
+const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    // {path: '', component: HomeComponentComponent},
+    {path: 'home', component: HomeComponentComponent},
+    {path: 'about', component: AboutComponentComponent},
+    {path: 'contact', component: ContactComponentComponent},
+    {path: 'contactUs', redirectTo: 'contact'},
+];
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponentComponent,
+        AboutComponentComponent,
+        ContactComponentComponent
     ],
     imports: [
         BrowserModule,
-        HttpModule
+        HttpModule,
+        RouterModule.forRoot(routes)
     ],
     providers: [
     ],

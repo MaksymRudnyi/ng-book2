219a682fedf4ce66faeaca40fbd9bc03df612944
maxim <maxim@webro.com.ua>
2017-01-02 21:56:09 +0200

    clear code for routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,17 @@ import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 
 import {AppComponent} from './app.component';
-import {
-    YouTubeSearchComponent,
-    SearchBox,
-    SearchResultComponent,
-    YouTubeService,
-    youTubeServiceInjectables
-} from './you-tube-search-component/you-tube-search-component.component';
+
 
 @NgModule({
     declarations: [
-        AppComponent,
-        YouTubeSearchComponent,
-        SearchResultComponent,
-        SearchBox
+        AppComponent
     ],
     imports: [
         BrowserModule,
         HttpModule
     ],
     providers: [
-        YouTubeService,
-        youTubeServiceInjectables
     ],
     bootstrap: [AppComponent]
 })

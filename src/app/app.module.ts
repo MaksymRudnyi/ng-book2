e31b259fbf2955c9b91787949ac723cc256a6f73
maxim <maxim@webro.com.ua>
2017-01-02 22:32:53 +0200

    clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,25 +8,15 @@ import {
     RouterModule,
     Routes
 } from '@angular/router';
-import { HomeComponentComponent } from './home-component/home-component.component';
-import { AboutComponentComponent } from './about-component/about-component.component';
-import { ContactComponentComponent } from './contact-component/contact-component.component'
 
 const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
-    // {path: '', component: HomeComponentComponent},
-    {path: 'home', component: HomeComponentComponent},
-    {path: 'about', component: AboutComponentComponent},
-    {path: 'contact', component: ContactComponentComponent},
     {path: 'contactUs', redirectTo: 'contact'},
 ];
 
 @NgModule({
     declarations: [
-        AppComponent,
-        HomeComponentComponent,
-        AboutComponentComponent,
-        ContactComponentComponent
+        AppComponent
     ],
     imports: [
         BrowserModule,
